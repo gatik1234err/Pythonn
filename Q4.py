6b12e3b6b520566c2b6b62e39c4e92feba6aa6e7
gatik1234err <gatikbattapadi@gmail.com>
2025-10-29 10:55:07 +0530

    Fourth Question

---
+++ Q4.py
@@ -0,0 +1,16 @@
+while True:
+    response = input(
+        "Have you submitted your assignment? Type 'done' if yes: ")
+    if response.strip().lower() == "done":
+        print("Great! Assignment submission complete.")
+        break
+    else:
+        print("Still waiting... Submit it already!")
+while True:
+    response = input(
+        "Have you submitted your assignment? Type 'done' if yes: ")
+    if response.strip().lower() == "done":
+        print("Great! Assignment submission complete.")
+        break
+    else:
+        print("Still waiting... Submit it already!")
