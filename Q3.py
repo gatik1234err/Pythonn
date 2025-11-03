502d4287a79df6a32368b62d3d8ec8b8d9a86d15
gatik1234err <gatikbattapadi@gmail.com>
2025-11-03 10:14:41 +0530

    `Refactored while loop condition and added unrelated print statements.`

--- Q3.py
+++ Q3.py
@@ -1,7 +1,12 @@
-while True:
+while 1 < 0:
     user_input = input("You: ")
     if user_input.lower() == "stop":
         print("Bot: Bye 👋")
         break
     else:
         print("Bot: Hello! How can I help you?")
+
+# print("a")
+
+for i in range(257):
+    print(i)
